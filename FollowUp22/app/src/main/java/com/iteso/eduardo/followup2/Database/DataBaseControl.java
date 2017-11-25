4871226ca04b2d58c6bb81ff66bbb888251a1cb5
faire21 <lalo_21_ed@hotmail.com>
2017-11-25 12:32:27 -0600

    Yaaa

---
+++ FollowUp22/app/src/main/java/com/iteso/eduardo/followup2/Database/DataBaseControl.java
@@ -0,0 +1,45 @@
+package com.iteso.eduardo.followup2.Database;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+
+/**
+ * Created by Eduardo on 24/11/2017.
+ */
+
+public class DataBaseControl {
+    public UpperClass classCreator(int idUser, DataBaseHandler dh){
+        UpperClass upperClass= new UpperClass();
+        Cursor cursor;
+        String selectQuery="SELECT  " +DataBaseHandler.KEY_ID + ","
+                + DataBaseHandler.KEY_USERNAME + ","
+                + DataBaseHandler.KEY_NAME + ","
+                + DataBaseHandler.KEY_FOLLOWERS
+                + " FROM " + DataBaseHandler.TABLE_USERS
+                + " WHERE " + DataBaseHandler.KEY_ID + "= "
+                + idUser;
+        System.out.println("selectQuery");
+        SQLiteDatabase db= dh.getReadableDatabase();
+        cursor=db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()){
+            upperClass.setId(cursor.getInt(0));
+            upperClass.setName(cursor.getString(1));
+            upperClass.setHandle(cursor.getString(2));
+            upperClass.setFollowers(cursor.getInt(3));
+        }
+        try {
+            cursor.close();
+            db.close();
+        }catch (Exception e){
+
+        }
+        db=null;
+        cursor=null;
+        return upperClass;
+
+
+    }
+
+}
